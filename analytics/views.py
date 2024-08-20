41bf7ce8700f8d4e021c038bff59c10c85fc95eb
Yusuf Berkay Girgin <berkay.girgin@usersdot.com>
2024-08-20 14:47:58 +0300

    status distribution viewset created, using all data from db

--- analytics/views.py
+++ analytics/views.py
@@ -24,16 +24,14 @@ class DataViewSet(viewsets.ViewSet):
         }
         return Response(response)
 
-class StatusDistributionView(APIView):
-    def get(self, request):
-        data = pd.DataFrame(list(Data.objects.all().values()))
-        status_distribution = (
-            data.groupby(["status", "type"])
-            .agg({"revenue": "sum", "conversions": "sum"})
-            .reset_index()
-        )
-        response = status_distribution.to_dict(orient="records")
-        return Response(response, status=status.HTTP_200_OK)
+    @action(detail=False, methods=['get'], url_path='status-distribution')
+    def status_distribution(self, request):
+        data = self.queryset
+        serializer = DataSerializer(data, many=True)
+        df = pd.DataFrame(serializer.data)
+        status_distribution = df.groupby(['status', 'type']).agg({'revenue': 'sum', 'conversions': 'sum'}).reset_index()
+        response = status_distribution.to_dict(orient='records')
+        return Response(response)
 
 
 class CategoryTypePerformanceView(APIView):
